c8c3cb2c9b69865967bb7204b7ff27a20600163e
selvaraj-kuppusamy <velmanikvps@gmail.com>
2020-12-13 23:08:22 +0530

    mutitasking with single command

--- MyThread.java
+++ MyThread.java
@@ -1,19 +1,32 @@
 public class MyThread implements Runnable
 {
-	public void run()
-	{
-		System.out.println("New thread running");
-		for(int i=1;i<=5;i++)
-		{
-			System.out.println(i);
-		}
-		System.out.println(Thread.currentThread());
-	}
-	public static void main(String[] args)
-	{
-		System.out.println("Main thread running");
-		MyThread th = new MyThread();
-		Thread t = new Thread(th);
-		t.start();
-	}
+int a = 100, b = 50 ;
+public void run()
+{
+addition();
+subtraction();
+multiplication();
+}
+void addition()
+{
+int sum = a + b;
+System.out.println("Addition of two numbers:" +sum);
+}
+void subtraction()
+{
+int sub = a - b;
+System.out.println("Subtraction of two numbers: " +sub);
+}
+void multiplication()
+{
+int multiply = a * b;
+System.out.println("Multiplication of two numbers: "+multiply);
+}
+public static void main(String[] args)
+{
+System.out.println("Main thread running");
+MyThread th = new MyThread();
+Thread t = new Thread(th);
+t.start();
+}
 }
