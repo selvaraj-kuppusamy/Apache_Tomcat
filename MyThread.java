c3a62fa5767a6e955fd13df33d1e40c40f56d708
selvaraj-kuppusamy <velmanikvps@gmail.com>
2020-12-13 22:52:45 +0530

    mutithreading in runnableinterface

--- MyThread.java
+++ MyThread.java
@@ -1,13 +1,18 @@
-public class MyThread extends Thread
+public class MyThread implements Runnable
 {
 	public void run()
 	{
-		System.out.println("New thread running");;
+		System.out.println("New thread running");
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println(i);
+		}
+		System.out.println(Thread.currentThread());
 	}
 	public static void main(String[] args)
 	{
 		System.out.println("Main thread running");
-		MyThread th =new MyThread();
+		MyThread th = new MyThread();
 		Thread t = new Thread(th);
 		t.start();
 	}
